887778a82adf275778484acf2cdabc31913f5576
TomekMichalski <tmm.michalski@gmail.com>
2019-03-11 20:20:43 +0100

    Recipe list and api calls

---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'c7ca22a89fd43630462f20b080e95b2d';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(this.result);
+        } catch (error){
+            alert(error)
+        }
+        
+    }
+}
+

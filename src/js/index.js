e99ac60259803cae00a9bac6d1708dd1c7f954dd
TomekMichalski <tmm.michalski@gmail.com>
2019-04-07 18:11:46 +0200

    Implementing list model, fixed a problem where could read count in renderResults.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, clearLoader, renderLoader } from './views/base';
@@ -94,7 +95,8 @@ const controlRecipe = async () => {
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
 //Handling recipe button clicks
-elements.recipe.addEventListener("click", e => {
+elements.recipe.addEventListener('click', e => {
+    console.log(state.recipe);
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
         if (state.recipe.servings > 1) {
             //decrease button is clicked
@@ -106,5 +108,7 @@ elements.recipe.addEventListener("click", e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     }
-    console.log(state.recipe);
 });
+
+const l = new List();
+console.log(l);
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -3,20 +3,21 @@ import { Fraction } from 'fractional';
 
 export const clearRecipe = () => {
     elements.recipe.innerHTML = '';
-}
+};
 const formatCount = count => {
     if (count) {
-        const [int, dec] = count.toString().split('.').map(el => parseInt(el, 10));
+        const newCount = Math.round(count * 10000) / 10000;
+        const [int, dec] = newCount.toString().split('.').map(el => parseInt(el, 10));
 
-        if (!dec) return count;
+        if (!dec) return newCount;
+
         if (int === 0) {
-            const fr = new Fraction(count);
+            const fr = new Fraction(newCount);
             return `${fr.numerator}/${fr.denominator}`;
         } else {
-            const fr = new Fraction(count - int);
+            const fr = new Fraction(newCount - int);
             return `${int} ${fr.numerator}/${fr.denominator}`;
         }
-
     }
     return '?';
 };
@@ -33,18 +34,16 @@ const createIngredient = ingredient => `
         ${ingredient.ingredient}
     </div>
     </li>
-    `
-;
+    `;
 
 export const renderRecipe = recipe => {
     const markup = `
         <figure class="recipe__fig">
-        <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
-        <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-        </h1>
+            <img src="${recipe.img}" alt="${recipe.title}" class="recipe__img">
+            <h1 class="recipe__title">
+                <span>${recipe.title}</span>
+            </h1>
         </figure>
-
         <div class="recipe__details">
             <div class="recipe__info">
                 <svg class="recipe__info-icon">
@@ -59,7 +58,6 @@ export const renderRecipe = recipe => {
                 </svg>
                 <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
                 <span class="recipe__info-text"> servings</span>
-
                 <div class="recipe__info-buttons">
                     <button class="btn-tiny btn-decrease">
                         <svg>
@@ -72,7 +70,6 @@ export const renderRecipe = recipe => {
                         </svg>
                     </button>
                 </div>
-
             </div>
 
             <button class="recipe__love">
@@ -81,27 +78,13 @@ export const renderRecipe = recipe => {
                 </svg>
             </button>
         </div>
-
-
-
         <div class="recipe__ingredients">
             <ul class="recipe__ingredient-list">
 
                 ${recipe.ingredients.map(el => createIngredient(el)).join('')}
+			</ul>
 
-
-                <li class="recipe__item">
-                    <svg class="recipe__icon">
-                        <use href="img/icons.svg#icon-check"></use>
-                    </svg>
-                    <div class="recipe__count">1/2</div>
-                    <div class="recipe__ingredient">
-                        <span class="recipe__unit">cup</span>
-                        ricotta cheese
-                    </div>
-                </li>
-
-            <button class="btn-small recipe__btn">
+            <button class="btn-small recipe__btn recipe__btn--add">
                 <svg class="search__icon">
                     <use href="img/icons.svg#icon-shopping-cart"></use>
                 </svg>
@@ -133,5 +116,5 @@ export const updateServingsIngredients = recipe => {
     const countElements = Array.from(document.querySelectorAll('.recipe__count'));
     countElements.forEach((el, i) => {
         el.textContent = formatCount(recipe.ingredients[i].count);
-    })
-}
+    });
+};
